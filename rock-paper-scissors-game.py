08fd60915c95d93a1c68e3d49b6f82324e6126f6
RAljowair <rayanaljowair@gmail.com>
2020-11-03 11:31:34 +0300

    enhance name exception handling

--- rock-paper-scissors-game.py
+++ rock-paper-scissors-game.py
@@ -1,7 +1,7 @@
 import random # importing random library
 import time # importing time library
 
-
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 tools = {1: "Rock", 2: "Paper", 3: "Scissors"}
 
 
@@ -10,7 +10,6 @@ def print_pulse(string):
     time.sleep(1)
     print(string)
     
-
 
 def winning_message():
     print_pulse("\nWinner Winner Chicken Dinner :)")
@@ -22,38 +21,46 @@ def losing_message():
     print_pulse("Better luck next time ;)")
 
 
-# game intro 
+# game introduction message and taking the player's name 
 def start_message():
     print_pulse("Hello")
     print_pulse("Welcome to Rock, Paper, Scissors game :)")
-    try:
-        player_name = str(input("First what's Your name: "))
-    except:
-        print("Your name should be only letters!")
-    print_pulse("Helllooo " + player_name)
+    # Checking if there is a numbers or symbols
+    while True:
+        try:
+            player_name = str(input("First what's Your name: "))
+            if any(char.isdigit() for char in player_name) or any(char.isalnum() for char in player_name):
+                raise Exception
+            else:
+                break
+        except:
+            print("Your name should be only letters!")
+    print_pulse("Hello " + player_name)
 
 
-# to select chices form the user and computer
+# to select choices form the user and computer
 def choices():
     print_pulse("Choose the number of which tool you want:")
     print_pulse("1.Rock")
     print_pulse("2.Paper")
     print_pulse("3.Scissors")
-    print_pulse("-> ")
     while True:
         try:
-            player_response = int(input())
-            break
-        except:
+            player_response = int(input(">"))
             if player_response > 3 or player_response < 1:
-                raise print_pulse("You mmust enter one of the three numbers above!") 
-            print("You must enter Only Numbers")
-
+                print("You must enter one of these numbers [1, 2, 3]:")
+            else:
+                break
+        except:
+                print("You must enter Only Numbers try again:")
     player_choice = tools[player_response]
     print_pulse(f"Your choice is: {player_choice}")
     random_num = random.randint(1, 3)
     computer_choice = tools[random_num] 
     print_pulse(f"Computer choice is: {computer_choice}")
+
+
+    # to compare tools and who will win
     def compare_choices():
         if player_choice.lower() == "rock" and computer_choice.lower() == "paper":
             losing_message()
@@ -70,7 +77,11 @@ def choices():
         elif player_choice.lower() == computer_choice.lower():
             print_pulse("\nDRAW!!!")
     compare_choices()
-        
 
-start_message()
-choices()
+
+def game():
+    start_message()
+    choices()
+
+
+game()
